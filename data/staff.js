f0f82e3009e68e78ce14f3b5ad1accf203228761
kashish-jainn <68510272+kashish-jainn@users.noreply.github.com>
2026-09-22 03:53:09 +0530

    Update staff.js
    Adding project staff

--- data/staff.js
+++ data/staff.js
@@ -11,23 +11,114 @@
 window.LAB = window.LAB || {};
 window.LAB.staff = [
   {
-    "name": "",
+    "name": "Mohd. Imran",
     "status": "current",
-    "designation": "",
+    "designation": "Project Manager",
     "photo": "",
-    "joined": "",
+    "joined": "Jun-2017",
     "left": "",
     "research": "",
     "currentRole": ""
   },
   {
-    "name": "",
-    "status": "past",
-    "designation": "",
+    "name": "Rahul Kumar",
+    "status": "current",
+    "designation": "Project Assistant (Technical)",
     "photo": "",
-    "joined": "",
+    "joined": "Jun-2017",
     "left": "",
     "research": "",
     "currentRole": ""
+  },
+  {
+    "name": "Mehbub-Un-Nabi",
+    "status": "current",
+    "designation": "Project Assistant (Technical)",
+    "photo": "",
+    "joined": "Jan-2021",
+    "left": "",
+    "research": "",
+    "currentRole": ""
+  },
+  {
+    "name": "Dr. Himanshu Lal",
+    "status": "past",
+    "designation": "National Post-Doc Fellow",
+    "photo": "",
+    "joined": "2016",
+    "left": "2018",
+    "research": "Personal exposure and pulmonary health effects assessment through panel studies",
+    "currentRole": "Principal Project Scientist, CAS, IIT Delhi"
+  },
+  
+  {
+    "name": "Late Dr. Bipasha Ghosh",
+    "status": "past",
+    "designation": "Institute Post-Doc Fellow",
+    "photo": "",
+    "joined": "2018",
+    "left": "2021",
+    "research": "Organic aerosol characterisation in ambient air",
+    "currentRole": ""
+  },
+  {
+    "name": "Dr. Mahak Bansal",
+    "status": "past",
+    "designation": "Project Post-Doc Fellow",
+    "photo": "",
+    "joined": "2018",
+    "left": "2023",
+    "research": "Emission inventory and source apportionment",
+    "currentRole": ""
+  },
+  {
+    "name": "Swati Joshi",
+    "status": "past",
+    "designation": "Junior Research Fellow",
+    "photo": "",
+    "joined": "2019",
+    "left": "2022",
+    "research": "Characterisation of inorganic species in ambient air",
+    "currentRole": "Ph.D. scholar, Delhi University"
+  },
+  {
+    "name": "Niraj Kumar",
+    "status": "past",
+    "designation": "Senior Research Fellow",
+    "photo": "",
+    "joined": "2023",
+    "left": "2026",
+    "research": "On-field emissions measurements and chemical characterisation",
+    "currentRole": "Senior Research Fellow, IIT Delhi"
+  },
+  {
+    "name": "Nisar Ali Baig",
+    "status": "past",
+    "designation": "Junior Research Fellow",
+    "photo": "",
+    "joined": "2017",
+    "left": "2019",
+    "research": "Exposure assessment and health measurements",
+    "currentRole": ""
+  },
+  {
+    "name": "Mohammad Yawar",
+    "status": "past",
+    "designation": "Junior Research Fellow",
+    "photo": "",
+    "joined": "2017",
+    "left": "2019",
+    "research": "Epidemiological modelling",
+    "currentRole": "Senior Data Analyst, McKinsey & Co."
+  },
+  {
+    "name": "Aisha Baig Warsi",
+    "status": "past",
+    "designation": "Project Manager",
+    "photo": "",
+    "joined": "2016",
+    "left": "2025",
+    "research": "",
+    "currentRole": "Project Manager, CAS, IIT Delhi"
   }
 ];
