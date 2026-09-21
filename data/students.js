0d8d89c2e4e9c026aa94fbcf74f2fcac615df41d
kashish-jainn <68510272+kashish-jainn@users.noreply.github.com>
2026-09-22 00:49:52 +0530

    Update students.js
    Added PhD details

--- data/students.js
+++ data/students.js
@@ -6,29 +6,29 @@ window.LAB.students = [
     "name": "Mohd Haneef",
     "status": "current",
     "photo": "",
-    "joined": "",
+    "joined": "Jun-2018",
     "graduated": "",
     "expectedGraduation": "",
-    "thesis": "",
+    "thesis": "Role of aerosol chemical composition in hygroscopic behaviour and optical properties during winter in Delhi",
     "affiliation": ""
   },
   {
     "name": "Mohd Shahzar Khan",
     "status": "current",
     "photo": "",
-    "joined": "",
+    "joined": "Jun-2019",
     "graduated": "",
     "expectedGraduation": "",
-    "thesis": "",
+    "thesis": "Emission characterization and inventory development of aerosol and its precursors from on-road transport sector",
     "affiliation": ""
   },
   {
-    "name": "Mohd Yousuf Subbair",
+    "name": "Mohd Yousuf Subair",
     "status": "current",
     "photo": "",
-    "joined": "",
+    "joined": "Jan-2023",
     "graduated": "",
-    "expectedGraduation": "",
+    "expectedGraduation": "Investigating the contribution of organics in refining source apportionment",
     "thesis": "",
     "affiliation": ""
   },
@@ -36,7 +36,7 @@ window.LAB.students = [
     "name": "Karegowda",
     "status": "current",
     "photo": "",
-    "joined": "",
+    "joined": "July-2023",
     "graduated": "",
     "expectedGraduation": "",
     "thesis": "",
@@ -46,7 +46,7 @@ window.LAB.students = [
     "name": "Himanshu Jhariya",
     "status": "current",
     "photo": "",
-    "joined": "",
+    "joined": "July-2025",
     "graduated": "",
     "expectedGraduation": "",
     "thesis": "",
@@ -56,7 +56,7 @@ window.LAB.students = [
     "name": "Aiman Kafeel",
     "status": "current",
     "photo": "",
-    "joined": "",
+    "joined": "July-2025",
     "graduated": "",
     "expectedGraduation": "",
     "thesis": "",
@@ -66,7 +66,7 @@ window.LAB.students = [
     "name": "Shobha Meena",
     "status": "current",
     "photo": "",
-    "joined": "",
+    "joined": "July-2025",
     "graduated": "",
     "expectedGraduation": "",
     "thesis": "",
@@ -77,89 +77,99 @@ window.LAB.students = [
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Jun-2017",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Emissions of aerosol and gases from on-road light duty vehicles",
+    "affiliation": "Assistant Professor, Central University of Rajasthan"
   },
   {
     "name": "Gaurav Singh",
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Oct-2018",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
-  },
-  {
-    "name": "Annada Padhi",
-    "status": "alumni",
-    "photo": "",
-    "joined": "",
-    "graduated": "",
-    "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Role of aerosols on cardiopulmonary functions of economically different groups of megacity Delhi and a town Hamirpur Himachal Pradesh",
+    "affiliation": "Assistant Engineer, Directorate of Local Bodies, Govt. of Rajasthan"
   },
   {
     "name": "Ananya Das",
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Sep-2020",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Effects of fine aerosol and its toxic components on human lung- An integrative approach",
+    "affiliation": "Assistant Vice President, Barclays"
   },
   {
     "name": "Zainab Arub",
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Jan-2022",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Investigation of aerosol hygroscopicity, its causative factors and impact at an urban site",
+    "affiliation": "Project Scientist, Indian Institute of Technology Delhi"
+  },
+  {
+    "name": "Annada Padhi",
+    "status": "alumni",
+    "photo": "",
+    "joined": "",
+    "graduated": "Feb-2022",
+    "expectedGraduation": "",
+    "thesis": "In field emission of aerosol and gases from traditional cook stove and its climate implication",
+    "affiliation": "Research Associate, Indian Institute of Tropical Meteorology"
   },
   {
     "name": "Lovleen Gupta",
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Oct-2022",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Source Apportionment of Aerosols in Ghaziabad using Advanced Receptor Modelling Tools",
+    "affiliation": "Associate Professor, Delhi Technological University"
   },
   {
     "name": "Sayantee Roy",
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Jul-2023",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Investigation of climate relevant properties of aerosol and contribution of sources in arid region, Bikaner, India",
+    "affiliation": "Post-Doctoral Researcher, NASA Langley Research Center"
   },
   {
     "name": "Jyoti Kumari",
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Sep-2025",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Aerosol emission inventory for residential sector of India based on fuel-use survey and on-field emission measurement",
+    "affiliation": "Technical Advisor, Pure Earth India Pvt. Ltd."
   },
   {
     "name": "Kashish Jain",
     "status": "alumni",
     "photo": "",
     "joined": "",
-    "graduated": "",
+    "graduated": "Apr-2026",
     "expectedGraduation": "",
-    "thesis": "",
-    "affiliation": ""
+    "thesis": "Short-term exposure to traffic-related air pollution and its effect on cardiovascular health",
+    "affiliation": "Early-Doc Fellow, Indian Institute of Technology Delhi"
   }
+  {
+    "name": "Diljit Kumar Nayak",
+    "status": "alumni",
+    "photo": "",
+    "joined": "",
+    "graduated": "Jun-2026",
+    "expectedGraduation": "",
+    "thesis": "Role of land-use land-cover change on meteorology and its effect on short lived climated pollutants",
+    "affiliation": "Independent Consultant"
+  },
 ];
